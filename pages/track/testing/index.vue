62c1c91d9d366394481409bffce73a110b04dc8e
Brianna Chen <blchen@mitre.org>
2021-03-31 17:32:12 -0400

    Remove router specifications for used for demo branch

--- pages/track/testing/index.vue
+++ pages/track/testing/index.vue
@@ -45,8 +45,7 @@ export default {
   },
   methods: {
     async createSurvey() {
-      const base = process.env.NODE_ENV === 'production' ? '/astute-canary/' : ''
-      const url = base + '/survey-configs/testing.json'
+      const url = '/survey-configs/testing.json'
       axios.get(url)
       .then(response => {
           this.json = response.data
