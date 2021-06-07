7c940c080961eb3c640dc6d9209ac51972208f10
Brianna Chen <blchen@mitre.org>
2021-06-07 09:45:36 -0400

    Updates

--- _nuxt/static/1622838392/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Flogin","\u002Ftrack\u002Fsymptoms","\u002Fprofile","\u002Ftrack","\u002Fregister","\u002Ftrends","\u002Ftrack\u002Ftesting","\u002Ftrack\u002Fvaccination","\u002Fhistory"]})
