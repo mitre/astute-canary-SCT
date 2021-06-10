5d9cb3c72da4274b6470594d06e3b4dc9deb2c2b
Brianna Chen <blchen@mitre.org>
2021-06-10 11:56:04 -0400

    Clean up UI in light mode

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -1,23 +1,27 @@
 <template>
-  <div class="bg-page-pattern bg-no-repeat bg-cover bg-right-top w-full min-h-screen flex flex-col items-center pb-24">
-    <div class="my-auto max-w-sm text-center">
-      <img src="~/assets/images/Logo.svg" width="100" class="mx-auto opacity-75 mb-8"/>
-      <h1 class="uppercase text-3xl text-light-text font-medium tracking-widest">{{ appName }}</h1>
-      <h2 class="text-light-text font-thin">Welcome back! Please login to your account</h2>
-      <input type="text" class="bg-transparent border-b w-full mt-8 pb-4 text-light-text text-sm focus:outline-none" v-model="username" placeholder="Username">
-      <input type="password" class="bg-transparent border-b w-full mt-8 pb-4 text-light-text text-sm focus:outline-none" v-model="password" placeholder="Password">
+  <div class="w-full min-h-screen flex flex-col items-center pb-24">
+    <div class="w-full bg-page-pattern bg-no-repeat bg-cover bg-bottom">
+      <div class="mx-auto max-w-sm text-center  w-full pt-8 pb-24">
+        <img src="~/assets/images/Logo.svg" width="100" class="mx-auto mb-8"/>
+        <h1 class="uppercase text-3xl text-primary font-medium tracking-widest">{{ appName }}</h1>
+      </div>
+    </div>
+    <div class="max-w-sm mx-auto flex flex-col flex-grow h-full w-full mt-4">
+      <h2 class="text-dark-text font-thin text-center">Welcome back! Please login to your account</h2>
+      <input type="text" class="bg-transparent border-b w-full mt-8 pb-4 text-dark-text text-sm focus:outline-none" v-model="username" placeholder="Username">
+      <input type="password" class="bg-transparent border-b w-full mt-8 pb-4 text-dark-text text-sm focus:outline-none" v-model="password" placeholder="Password">
       <div class="flex flex-row justify-between mt-4">
         <div class="flex flex-row items-center">
-          <input type="checkbox" v-model="rememberMe" name="remember"> <label for="remember" class="text-light-text ml-2 text-sm">Remember me</label>
+          <input type="checkbox" v-model="rememberMe" name="remember"> <label for="remember" class="text-dark-text ml-2 text-sm">Remember me</label>
         </div>
-        <a href="#" class="text-light-text underline font-thin text-sm">Forgot Password</a>
+        <a href="#" class="text-dark-text underline font-thin text-sm">Forgot Password</a>
       </div>
       <div class="flex flex-row w-full justify-center mt-8">
         <app-button type="secondary" @clicked="login" class="w-full">Login</app-button>
       </div>
       <div class="flex flex-row w-full justify-center items-center mt-8">
-        <span class="text-sm text-light-text">Don't have an account?</span>
-        <app-button class="ml-2" textType="light-text" type="" border="true" borderType="secondary" @clicked="register">Sign Up</app-button>
+        <span class="text-sm text-dark-text">Don't have an account?</span>
+        <app-button class="ml-2" textType="dark-text" type="" border="true" borderType="secondary" @clicked="register">Sign Up</app-button>
       </div>
       <div class="mt-8">
         <app-powered-by-statement></app-powered-by-statement>
--- pages/register/index.vue
+++ pages/register/index.vue
@@ -1,29 +1,31 @@
 <template>
-  <div class="bg-page-pattern bg-no-repeat bg-cover bg-right-top w-full h-screen flex flex-col items-center">
-    <div class="my-auto text-center mx-auto">
-      <img src="~/assets/images/Logo.svg" width="100" class="mx-auto opacity-50 mb-8"/>
-      <h1 class="uppercase text-3xl text-light-text font-bold tracking-widest">{{ appName }}</h1>
-
-      <h3 class="text-3xl text-light-text font-thin mt-8">Sign Up</h3>
-
+  <div class="w-full h-screen flex flex-col items-center">
+    <div class="w-full bg-page-pattern bg-no-repeat bg-cover bg-bottom">
+      <div class="mx-auto max-w-sm text-center  w-full pt-8 pb-24">
+        <img src="~/assets/images/Logo.svg" width="100" class="mx-auto mb-8"/>
+        <h1 class="uppercase text-3xl text-dark-text font-bold tracking-widest">{{ appName }}</h1>
+        <h3 class="text-3xl text-dark-text font-thin mt-8">Sign Up</h3>
+      </div>
+    </div>
+    <div class="max-w-sm mx-auto flex flex-col flex-grow h-full w-full mt-4">
       <form>
         <fieldset>
           <div class="mt-4">
-            <label class="text-gray-300" for="username">Enter email or username</label>
+            <label class="text-gray-600" for="username">Enter email or username</label>
             <input type="text" name="username" class=" border rounded w-full py-2 pl-1 text-sm focus:outline-none" v-model="username" placeholder="Username">
           </div>
           <div class="mt-4">
-            <label class="text-gray-300" for="password">Enter password</label>
+            <label class="text-gray-600" for="password">Enter password</label>
             <input type="text" name="password" class=" border rounded w-full py-2 pl-1 text-sm focus:outline-none" v-model="password" placeholder="Password">
           </div>
           <div class="mt-4">
-            <label class="text-gray-300" for="confirmPassword">Confirm password</label>
+            <label class="text-gray-600" for="confirmPassword">Confirm password</label>
             <input type="text" name="confirmPassword" class=" border rounded w-full py-2 pl-1 text-sm focus:outline-none" v-model="confirmPassword" placeholder="Password">
           </div>
         </fieldset>
      </form>
       <div class="flex flex-row justify-center mt-8">
-        <app-button type="white" textType="dark-text" @clicked="cancel" class="mr-8">Cancel</app-button>
+        <app-button type="white" border="true" textType="dark-text" @clicked="cancel" class="mr-8">Cancel</app-button>
         <app-button @clicked="register" type="secondary">Sign Up</app-button>
       </div>
     </div>
